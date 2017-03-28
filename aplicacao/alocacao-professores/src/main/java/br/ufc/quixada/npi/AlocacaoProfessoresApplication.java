106aee516be6f1eaf959bd95271a6579bae41f2b
wendellmendes <wendellmendes@172.18.22.25>
2017-03-28 08:32:45 -0300

    Configuração inicial do projeto com Spring Boot.

---
+++ aplicacao/alocacao-professores/src/main/java/br/ufc/quixada/npi/AlocacaoProfessoresApplication.java
@@ -0,0 +1,12 @@
+package br.ufc.quixada.npi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlocacaoProfessoresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlocacaoProfessoresApplication.class, args);
+	}
+}
