05c8664c91941ff834f7824e17917521a613696e
Caio Melo <caiomelo8@gmail.com>
2017-04-18 11:57:31 -0300

    #7 Adição das views de edição e cadastro de compartilhamentos.
    Incremento da view de cadastro de compartilhamento.
    
    Configuração de todos os @RequestMappings no CompartilhamentoController.

--- aplicacao/alocacao-professores/src/main/java/ufc/quixada/npi/ap/controller/CompartilhamentoController.java
+++ aplicacao/alocacao-professores/src/main/java/ufc/quixada/npi/ap/controller/CompartilhamentoController.java
@@ -1,6 +1,7 @@
 package ufc.quixada.npi.ap.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -9,7 +10,7 @@ import org.springframework.web.servlet.ModelAndView;
 @RequestMapping(path = "/compartilhamentos")
 public class CompartilhamentoController {
 	
-	@RequestMapping(path = {"", "/"}, method = RequestMethod.GET)
+	@RequestMapping(path = {""}, method = RequestMethod.GET)
 	public String listarCompartilhamentos(){
 		return "compartilhamento/listar-compartilhamento";
 	}
@@ -20,13 +21,31 @@ public class CompartilhamentoController {
 	}
 	
 	@RequestMapping(path = {"/cadastrar"}, method = RequestMethod.POST)
-	public ModelAndView cadastrarCompartilhamento(Integer turma, Integer oferta, Integer vagas){
-		ModelAndView model = new ModelAndView("redirect:/compartilhamentos/");
-		
-		String mensagem = "Cadastro realizado com sucesso!";
-		
-		model.addObject("mensagem", mensagem);
-		
+	public ModelAndView cadastrarCompartilhamento(Integer turma, Integer oferta, Integer numeroVagas){
+		ModelAndView model = new ModelAndView("redirect:/compartilhamentos?sucessoCadastro");
 		return model;
 	}
+	
+	@RequestMapping(path = {"/{id}/detalhar"}, method = RequestMethod.GET)
+	public String detalharCompartilhamento(@PathVariable(name = "id", required = true) Integer id){
+		
+		return "compartilhamento/detalhar-compartilhamento";
+	}
+	
+	@RequestMapping(path = {"/{id}/editar"}, method = RequestMethod.GET)
+	public String editarCompartilhamento(@PathVariable(name = "id", required = true) Integer id){
+		return "compartilhamento/editar-compartilhamento";
+	}
+	
+	@RequestMapping(path = {"/editar"}, method = RequestMethod.POST)
+	public ModelAndView editarCompartilhamento(Integer turma, Integer oferta, Integer numeroVagas){
+		ModelAndView model = new ModelAndView("redirect:/compartilhamentos?sucessoEdicao");
+		return model;
+	}
+	
+	@RequestMapping(path = {"/{id}/excluir"}, method = RequestMethod.GET)
+	public String excluirCompartilhamento(@PathVariable(name = "id", required = true) Integer id){
+		return "compartilhamento/excluir-compartilhamento";
+	}
+	
 }
