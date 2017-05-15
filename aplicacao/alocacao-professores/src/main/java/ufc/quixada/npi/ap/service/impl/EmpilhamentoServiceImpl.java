1ff4da7f239a98ae71e483d4dd1212657868309d
Phellipehz <phellipeh@alu.ufc.br>
2017-05-15 08:28:06 -0300

    #14 Ajustes no código

--- aplicacao/alocacao-professores/src/main/java/ufc/quixada/npi/ap/service/impl/EmpilhamentoServiceImpl.java
+++ aplicacao/alocacao-professores/src/main/java/ufc/quixada/npi/ap/service/impl/EmpilhamentoServiceImpl.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import ufc.quixada.npi.ap.model.Curso;
 import ufc.quixada.npi.ap.model.Disciplina;
 import ufc.quixada.npi.ap.model.Empilhamento;
 import ufc.quixada.npi.ap.model.Turma;
