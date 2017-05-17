eeb1c432559141d70579c82e5be366bbe8247607
Carlosps <carlosreisps@gmail.com>
2017-05-17 09:18:38 -0300

    #3 Implementação do listar Oferta

--- aplicacao/alocacao-professores/src/main/java/ufc/quixada/npi/ap/controller/OfertaController.java
+++ aplicacao/alocacao-professores/src/main/java/ufc/quixada/npi/ap/controller/OfertaController.java
@@ -1,53 +1,62 @@
 package ufc.quixada.npi.ap.controller;
 
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
-import ufc.quixada.npi.ap.util.Constants;
 
+import ufc.quixada.npi.ap.service.OfertaService;
+import ufc.quixada.npi.ap.util.Constants;
 
 @Controller
 @RequestMapping(path = "/ofertas")
 public class OfertaController {
-	
-	@RequestMapping(value = {""}, method = RequestMethod.GET)
-	public String listar(){
-		
-		return Constants.OFERTA_LISTAR;
+
+	@Autowired
+	public OfertaService ofertaService;
+
+	@RequestMapping(value = { "" }, method = RequestMethod.GET)
+	public ModelAndView listar() {
+
+		ModelAndView model = new ModelAndView(Constants.OFERTA_LISTAR);
+
+		model.addObject("ofertas", ofertaService.listar());
+
+		return model;
+
 	}
 
-	@RequestMapping(value = {"/cadastrar"}, method = RequestMethod.GET)
-	public String cadastrarOfertas(){
+	@RequestMapping(value = { "/cadastrar" }, method = RequestMethod.GET)
+	public String cadastrarOfertas() {
 
 		return Constants.OFERTA_CADASTRAR;
 	}
-	
-	@RequestMapping(value = {"/cadastrar"}, method = RequestMethod.POST)
-	public ModelAndView cadastrarOferta(){
+
+	@RequestMapping(value = { "/cadastrar" }, method = RequestMethod.POST)
+	public ModelAndView cadastrarOferta() {
 
 		ModelAndView model = new ModelAndView(Constants.OFERTA_LISTAR);
 		return model;
 	}
-	
+
 	@RequestMapping(value = "/{id}/editar")
-	public ModelAndView editar(){
+	public ModelAndView editar() {
 		ModelAndView mv = new ModelAndView(Constants.OFERTA_FORM_EDITAR);
 
 		return mv;
 	}
 
 	@RequestMapping(value = "/{id}/detalhar")
-	public ModelAndView detalhar(){
+	public ModelAndView detalhar() {
 		ModelAndView mv = new ModelAndView(Constants.OFERTA_DETALHAR);
 
 		return mv;
 	}
 
-	@RequestMapping(value= "/{id}/excluir")
-	public void excluir(){
-	
+	@RequestMapping(value = "/{id}/excluir")
+	public void excluir() {
+
 	}
 
 }
---
+++ aplicacao/alocacao-professores/src/main/java/ufc/quixada/npi/ap/service/OfertaService.java
@@ -0,0 +1,12 @@
+package ufc.quixada.npi.ap.service;
+
+import java.util.List;
+
+import ufc.quixada.npi.ap.model.Oferta;
+
+public interface OfertaService {
+	
+	List<Oferta> listar();
+	
+}
+
