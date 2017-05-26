1a87869c437c84df2e7af754c4de1aa8dd94f518
Caio <caiomelo8@gmail.com>
2017-05-26 03:09:16 -0300

    #7 Atualização do nome do pacote com as classes de validação
    Atualização do noe do pacote que contém as classes de validação, mudando
    de "ufc.quixa.npi.validation" para "ufc.quixada.npi.ap.validation".
    
    Remoção da anotação @ComponentScan adicionada anteriormente à classe
    AlocacaoProfessoresApplication.

--- aplicacao/alocacao-professores/src/main/java/ufc/quixada/npi/ap/AlocacaoProfessoresApplication.java
+++ aplicacao/alocacao-professores/src/main/java/ufc/quixada/npi/ap/AlocacaoProfessoresApplication.java
@@ -2,10 +2,8 @@ package ufc.quixada.npi.ap;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"ufc.quixada.npi"})
 public class AlocacaoProfessoresApplication {
 
 	public static void main(String[] args) {
