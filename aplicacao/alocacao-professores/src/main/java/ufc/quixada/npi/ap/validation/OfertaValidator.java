9beb452be20de7e5f20313c1f8b32924ca364f5a
Caio Melo <caiomelo8@gmail.com>
2017-06-14 12:00:53 -0300

    #1 Adição da validação do cadastro de ofertas
    Pequena atualização na página de cadastrar-oferta.
    
    Adição da validação de todos os campos da página de cadastrar-oferta.
    
    Atualização da classe OfertaValidator, para validação dos campos da
    Oferta.
    
    Adição das mensagens de erro relativas à validação da Oferta, no arquivo
    messages.properties.
    
    Adição do modificador "public" no enum Turno, pertencente à classe
    Oferta.

--- aplicacao/alocacao-professores/src/main/java/ufc/quixada/npi/ap/validation/OfertaValidator.java
+++ aplicacao/alocacao-professores/src/main/java/ufc/quixada/npi/ap/validation/OfertaValidator.java
@@ -1,11 +1,17 @@
 package ufc.quixada.npi.ap.validation;
 
+import java.util.List;
+
 import javax.inject.Named;
 
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
 
+import ufc.quixada.npi.ap.model.Disciplina;
 import ufc.quixada.npi.ap.model.Oferta;
+import ufc.quixada.npi.ap.model.Oferta.Turno;
+import ufc.quixada.npi.ap.model.Professor;
+import ufc.quixada.npi.ap.model.Turma;
 
 @Named
 public class OfertaValidator implements Validator {
@@ -17,7 +23,43 @@ public class OfertaValidator implements Validator {
 
 	@Override
 	public void validate(Object objeto, Errors erros) {
-		// TODO Auto-generated method stub
+		Oferta oferta = (Oferta) objeto;
+		
+		validateProfessoresNotNull(erros, oferta.getProfessores(), "professores", "professoresNull");
+		validateTurma(erros, oferta.getTurma(), "turma.id", "turmaNull");
+		validateDisciplina(erros, oferta.getDisciplina(), "disciplina.id", "disciplinaNull");
+		validateTurno(erros, oferta.getTurno(), "turno", "turnoNull");
+		validateVagasNotNull(erros, oferta.getVagas(), "vagas", "vagasNull");
+		validateVagasValorInvalido(erros, oferta.getVagas(), "vagas", "vagasInvalid");
 	}
-
+	
+	private void validateProfessoresNotNull(Errors erros, List<Professor> professores, String campo, String mensagem){
+		if (professores == null || professores.isEmpty())
+			erros.rejectValue(campo, mensagem);
+	}
+	
+	private void validateTurma(Errors erros, Turma turma, String campo, String mensagem){
+		if (turma == null || turma.getId() == null || turma.getId() <= 0)
+			erros.rejectValue(campo, mensagem);
+	}
+	
+	private void validateDisciplina(Errors erros, Disciplina disciplina, String campo, String mensagem){
+		if (disciplina == null || disciplina.getId() == null || disciplina.getId() <= 0)
+			erros.rejectValue(campo, mensagem);
+	}
+	
+	private void validateTurno(Errors erros, Turno turno, String campo, String mensagem){
+		if (turno == null)
+			erros.rejectValue(campo, mensagem);
+	}
+	
+	private void validateVagasNotNull(Errors erros, Integer vagas, String campo, String mensagem){
+		if (vagas == null)
+			erros.rejectValue(campo, mensagem);
+	}
+	
+	private void validateVagasValorInvalido(Errors erros, Integer vagas, String campo, String mensagem){
+		if (vagas != null && vagas <= 0)
+			erros.rejectValue(campo, mensagem);
+	}
 }
