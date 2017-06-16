1e281b5f8f61db51cc1dc968e313f6fff966ac63
Caio <caiomelo8@gmail.com>
2017-06-16 03:15:47 -0300

    #6 Adição da funcionalidade de exclusão de ofertas
    Integração da página listar-oferta com o template padrão. Além disso,
    adição da tabela para listagem de ofertas.
    
    Modificação de todos os métodos da classe OfertaController: adição dos
    tipos de requisição (RequestType.GET e RequestType.POST) de cada método,
    e modificação do nome do objeto da classe ModelAndView.
    
    Modificação no método GET para listagem de ofertas, para que ele
    adicione a lista de ofertas recuperada do banco no ModelAndView.
    
    Ainda na classe OfertaController, foi implementada a funcionalidade de
    excluir oferta. Para isso, foi necessária a definição do objeto da
    classe OfertaService.
    
    Adição da interface de repositório OfertaRepository.
    
    Adição da classe de service OfertaService, e da classe que a implementa,
    OfertaServiceImpl.
    
    Adição da String referente ao redirect para a página de listar-ofertas,
    na classe Constants.
    
    Adição do script oferta.js, que define as mensagens do SweetAlert
    referentes à exclusão de ofertas.
    
    Remoção do código desnecessário no script ordenacao-tabela.js.

--- aplicacao/alocacao-professores/src/main/java/ufc/quixada/npi/ap/controller/OfertaController.java
+++ aplicacao/alocacao-professores/src/main/java/ufc/quixada/npi/ap/controller/OfertaController.java
@@ -1,9 +1,21 @@
 package ufc.quixada.npi.ap.controller;
 
 
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
+
+import ufc.quixada.npi.ap.model.Oferta;
+import ufc.quixada.npi.ap.service.OfertaService;
 import ufc.quixada.npi.ap.util.Constants;
 
 
@@ -11,37 +23,62 @@ import ufc.quixada.npi.ap.util.Constants;
 @RequestMapping(path = "/ofertas")
 public class OfertaController {
 	
+	@Autowired
+	private OfertaService ofertaService;
+	
 	@RequestMapping(value = {"", "/"})
 	public ModelAndView listarOfertas(){
-		ModelAndView mv = new ModelAndView(Constants.OFERTA_LISTAR);
-
-		return mv;
+		ModelAndView modelAndView = new ModelAndView(Constants.OFERTA_LISTAR);
+		
+		modelAndView.addObject("ofertas", ofertaService.findAllOfertas());
+		
+		return modelAndView;
 	}
 
-	@RequestMapping(value = "/cadastrar")
-	public ModelAndView cadastrarOferta(){
-		ModelAndView mv = new ModelAndView(Constants.OFERTA_CADASTRAR);
+	@RequestMapping(value = "/cadastrar", method = RequestMethod.GET)
+	public ModelAndView cadastrarOferta(@ModelAttribute("oferta") Oferta oferta){
+		ModelAndView modelAndView = new ModelAndView(Constants.OFERTA_CADASTRAR);
+		
+		return modelAndView;
+	}
 
-		return mv;
+	@RequestMapping(value = "/cadastrar", method = RequestMethod.POST)
+	public ModelAndView cadastrarOferta(
+			@ModelAttribute("oferta") @Valid Oferta oferta,
+				BindingResult bindingResult, ModelAndView modelAndView){
+		
+		modelAndView.setViewName(Constants.OFERTA_REDIRECT_LISTAR);
+		
+		return modelAndView;
 	}
 	
-	@RequestMapping(value = "/{id}/editar")
-	public ModelAndView editarOferta(){
-		ModelAndView mv = new ModelAndView(Constants.OFERTA_EDITAR);
+	@RequestMapping(value = "/{id}/editar", method = RequestMethod.GET)
+	public ModelAndView editarOferta(@PathVariable("id") Integer id){
+		ModelAndView modelAndView = new ModelAndView(Constants.OFERTA_EDITAR);
 
-		return mv;
+		return modelAndView;
 	}
-
-	@RequestMapping(value = "/{id}/detalhar")
-	public ModelAndView detalharOferta(){
-		ModelAndView mv = new ModelAndView(Constants.OFERTA_DETALHAR);
-
-		return mv;
-	}
-
-	@RequestMapping(value= "/{id}/excluir")
-	public void excluirOferta(){
 	
+	@RequestMapping(value = "/{id}/editar", method = RequestMethod.POST)
+	public ModelAndView editarOferta(
+			@PathVariable(name = "id", required = true) Integer id,
+				@ModelAttribute("oferta") @Valid Oferta oferta, 
+					BindingResult bindingResult, ModelAndView modelAndView){
+		
+		modelAndView.setViewName(Constants.OFERTA_EDITAR);
+
+		return modelAndView;
+	}
+
+	@RequestMapping(value= "/{id}/excluir", method = RequestMethod.GET)
+	public @ResponseBody boolean excluirOferta(@PathVariable(name = "id", required = true) Integer id){
+		try {
+			ofertaService.excluir(id);
+		} catch (EmptyResultDataAccessException e) {
+			return false;
+		}
+		
+		return true;
 	}
 	
 }
---
+++ aplicacao/alocacao-professores/src/main/java/ufc/quixada/npi/ap/service/impl/OfertaServiceImpl.java
@@ -0,0 +1,28 @@
+package ufc.quixada.npi.ap.service.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import ufc.quixada.npi.ap.model.Oferta;
+import ufc.quixada.npi.ap.repository.OfertaRepository;
+import ufc.quixada.npi.ap.service.OfertaService;
+
+@Service
+public class OfertaServiceImpl implements OfertaService {
+	
+	@Autowired
+	private OfertaRepository ofertaRepository;
+
+	@Override
+	public List<Oferta> findAllOfertas() {
+		return ofertaRepository.findAll();
+	}
+	
+	@Override
+	public void excluir(Integer id) {
+		ofertaRepository.delete(id);
+	}
+
+}
