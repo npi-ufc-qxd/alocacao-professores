937dec0b4cba0c853bf0742438262df14f33f3d0
60027902331 <60027902331@npi-345913>
2017-06-26 09:51:57 -0300

    4-detalhes-oferta

--- aplicacao/alocacao-professores/src/main/java/ufc/quixada/npi/ap/controller/OfertaController.java
+++ aplicacao/alocacao-professores/src/main/java/ufc/quixada/npi/ap/controller/OfertaController.java
@@ -1,15 +1,26 @@
 package ufc.quixada.npi.ap.controller;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+
+import ufc.quixada.npi.ap.model.Empilhamento;
+import ufc.quixada.npi.ap.model.Oferta;
+import ufc.quixada.npi.ap.service.OfertaService;
 import ufc.quixada.npi.ap.util.Constants;
 
 
 @Controller
 @RequestMapping(path = "/ofertas")
 public class OfertaController {
+	
+	@Autowired
+	OfertaService ofertaService;
 	
 	@RequestMapping(value = {"", "/"})
 	public ModelAndView listarOfertas(){
@@ -32,11 +43,16 @@ public class OfertaController {
 		return mv;
 	}
 
-	@RequestMapping(value = "/{id}/detalhar")
-	public ModelAndView detalharOferta(){
-		ModelAndView mv = new ModelAndView(Constants.OFERTA_DETALHAR);
-
-		return mv;
+	@RequestMapping(path = {"/{id}/detalhar"}, method = RequestMethod.GET)
+	public ModelAndView detalharOferta(@PathVariable("id") Integer id, @RequestParam(required=false) String erro){
+		
+		Oferta oferta=  ofertaService.visualizarOferta(id);
+		
+		ModelAndView modelAndView = new ModelAndView(Constants.OFERTA_DETALHAR);
+		modelAndView.addObject("oferta", oferta);
+		modelAndView.addObject("professores",oferta.getProfessores());
+		modelAndView.addObject("erro", erro);
+		return modelAndView;
 	}
 
 	@RequestMapping(value= "/{id}/excluir")
---
+++ aplicacao/alocacao-professores/src/main/java/ufc/quixada/npi/ap/service/impl/OfertaServiceImpl.java
@@ -0,0 +1,38 @@
+package ufc.quixada.npi.ap.service.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import ufc.quixada.npi.ap.model.Oferta;
+import ufc.quixada.npi.ap.repository.OfertaRepository;
+import ufc.quixada.npi.ap.service.OfertaService;
+
+@Service
+public class OfertaServiceImpl implements OfertaService {
+
+	@Autowired
+	private OfertaRepository ofertaRepository;
+	
+	@Override
+	public Oferta salvarOferta(Oferta oferta) {
+		return ofertaRepository.save(oferta);
+	}
+
+	@Override
+	public List<Oferta> listarOfertas() {
+		return ofertaRepository.findAll();
+	}
+
+	@Override
+	public void excluirOferta(Integer idOferta) {
+		ofertaRepository.delete(idOferta);
+	}
+
+	@Override
+	public Oferta visualizarOferta(Integer idOferta) {
+		return ofertaRepository.findOne(idOferta);
+	}
+
+}
