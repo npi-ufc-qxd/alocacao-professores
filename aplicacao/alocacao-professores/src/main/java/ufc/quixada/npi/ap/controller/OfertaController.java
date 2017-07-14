808db1ca23b289a78aa4c9bb12c7219069405a96
Lucas Vieira <lucas.vss12@gmail.com>
2017-07-14 10:36:00 -0300

    Correção de bug na importação de oferta ocasionado pelo programador

--- aplicacao/alocacao-professores/src/main/java/ufc/quixada/npi/ap/controller/OfertaController.java
+++ aplicacao/alocacao-professores/src/main/java/ufc/quixada/npi/ap/controller/OfertaController.java
@@ -71,6 +71,7 @@ public class OfertaController {
 	@RequestMapping(value = { "", "/" }, method = RequestMethod.GET)
 	public ModelAndView listarOfertas() {
 		ModelAndView modelAndView = new ModelAndView(Constants.OFERTA_LISTAR);
+		modelAndView.addObject("periodos", periodoService.periodosConsolidados());
 		return modelAndView;
 	}
 	
