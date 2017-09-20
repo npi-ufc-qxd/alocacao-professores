bf56647c5e39d0b9cc5275ef9843661f34b9f591
Ederson Abreu <ederson.dimm@gmail.com>
2017-09-20 10:17:08 -0300

    implementaçao editar oferta direcao em andamento

--- aplicacao/alocacao-professores/src/main/java/ufc/quixada/npi/ap/controller/DirecaoController.java
+++ aplicacao/alocacao-professores/src/main/java/ufc/quixada/npi/ap/controller/DirecaoController.java
@@ -4,22 +4,32 @@ import static ufc.quixada.npi.ap.util.Constants.COMPARTILHAMENTO_LISTAR;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import br.ufc.quixada.npi.ldap.model.Usuario;
 import br.ufc.quixada.npi.ldap.service.UsuarioService;
+import ufc.quixada.npi.ap.exception.AlocacaoProfessoresException;
+import ufc.quixada.npi.ap.model.Oferta;
 import ufc.quixada.npi.ap.model.Pessoa;
 import ufc.quixada.npi.ap.model.Professor;
 import ufc.quixada.npi.ap.service.CompartilhamentoService;
+import ufc.quixada.npi.ap.service.DisciplinaService;
+import ufc.quixada.npi.ap.service.OfertaService;
 import ufc.quixada.npi.ap.service.PeriodoService;
 import ufc.quixada.npi.ap.service.PessoaService;
 import ufc.quixada.npi.ap.service.ProfessorService;
 import ufc.quixada.npi.ap.util.Constants;
+import ufc.quixada.npi.ap.validation.OfertaValidator;
 
 @Controller
 public class DirecaoController {
@@ -38,6 +48,15 @@ public class DirecaoController {
 
 	@Autowired
 	private PeriodoService periodoService;
+	
+	@Autowired
+	private OfertaService ofertaService;
+	
+	@Autowired
+	private DisciplinaService disciplinaService;
+	
+	@Autowired
+	private OfertaValidator ofertaValidator;
 	
 	@RequestMapping(path = {"/oferta-campus"}, method = RequestMethod.GET)
 	public String listarCompartilhamentos(Model model){
@@ -81,6 +100,45 @@ public class DirecaoController {
 			
 		}
 		
+		return modelAndView;
+	}
+	
+	@RequestMapping(value = "/editar-oferta/{id}", method = RequestMethod.GET)
+	public ModelAndView editarOferta(@PathVariable("id") Integer id) {
+		ModelAndView modelAndView = new ModelAndView(Constants.OFERTA_EDITAR_DIRECAO);
+
+		Oferta oferta = ofertaService.findOferta(id);
+		modelAndView.addObject("cursoAtual", oferta.getTurma().getCurso());
+
+		modelAndView.addObject("oferta", oferta);
+		modelAndView.addObject("disciplinas", disciplinaService.listarNaoArquivada());
+
+		return modelAndView;
+	}
+
+	@RequestMapping(value = "/editar-oferta/{id}", method = RequestMethod.POST)
+	public ModelAndView editarOferta(@PathVariable(name = "id", required = true) Integer id,
+			@ModelAttribute("oferta") @Valid Oferta oferta, BindingResult bindingResult, ModelAndView modelAndView) {
+
+		ofertaValidator.validate(oferta, bindingResult);
+
+		if (bindingResult.hasErrors()) {
+			modelAndView.addObject("cursoAtual", oferta.getTurma().getCurso());
+			modelAndView.setViewName(Constants.OFERTA_EDITAR_DIRECAO);
+			modelAndView.addObject("disciplinas", disciplinaService.listarNaoArquivada());
+
+			return modelAndView;
+		}
+
+		try {
+			ofertaService.salvar(oferta);
+		} catch (AlocacaoProfessoresException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		modelAndView.setViewName(Constants.OFERTA_REDIRECT_LISTAR);
+
 		return modelAndView;
 	}
 
--- aplicacao/alocacao-professores/src/main/java/ufc/quixada/npi/ap/util/Constants.java
+++ aplicacao/alocacao-professores/src/main/java/ufc/quixada/npi/ap/util/Constants.java
@@ -35,6 +35,7 @@ public class Constants {
 	public static final String OFERTA_REDIRECT_LISTAR = "redirect:/ofertas";
 	public static final String OFERTA_REDIRECT_CADASTRO = "redirect:/ofertas/cadastrar";
 	public static final String OFERTA_CADASTRADA = "Oferta cadastrada com sucesso!";
+	public static final String OFERTA_EDITAR_DIRECAO = "/ofertas/editar-oferta-direcao";
 	
 	// Compartilhamento
 	public static final String COMPARTILHAMENTO_CADASTRAR = "compartilhamento/cadastrar-compartilhamento";
